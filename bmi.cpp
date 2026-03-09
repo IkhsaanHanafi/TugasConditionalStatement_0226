882a947a53f5b149637131f73f726fcf3e7243f5
IkhsaanHanafi <ikhsaanhanafi@gmail.com>
2026-03-09 23:00:19 +0700

    menambahkan perintah input

--- bmi.cpp
+++ bmi.cpp
@@ -5,3 +5,10 @@ using namespace std;
 float hitung_bmi(float berat, float tinggi){
     return berat / (tinggi * tinggi);
 }
+
+void input_data(float berat, float tinggi){
+    cout << "Masukkan berat badan (Kg) = ";
+    cin >> berat;
+    cout << "Masukkan tinggi badan (m) = ";
+    cin >> tinggi;
+}
